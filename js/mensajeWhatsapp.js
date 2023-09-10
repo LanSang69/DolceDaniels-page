dd981dd979e75a2def0f285a67051ca90c7b2d6b
LanSang69 <alanisg029@gmail.com>
2023-09-10 10:52:19 -0600

    detalles finales de la página

--- js/mensajeWhatsapp.js
+++ js/mensajeWhatsapp.js
@@ -1,14 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {      //Se asegura que los elementos existan antes de hacer algo
-  const mensajeInput = document.getElementById('mensajeInput'); //Obtengo el ID del mesaje
-  const enviarButton = document.getElementById('enviarButton'); //Obtengo ID del botón
+document.addEventListener('DOMContentLoaded', function() {
+  // Asegura que los elementos existan antes de hacer algo
+  const mensajeInput = document.getElementById('mensajeInput'); // Obtengo el ID del mensaje
+  const enviarButton = document.getElementById('enviarButton'); // Obtengo ID del botón
 
   enviarButton.addEventListener('click', function(event) {
     event.preventDefault(); // Evita que el formulario se envíe por defecto
 
-    const mensaje = encodeURIComponent(mensajeInput.value);   //Copia el v
-    const numero = '+525564779982'; // Tu número de WhatsApp
+    iziToast.success({
+      title: 'Redirigiendo a WhatsApp...',
+      message: '¡Bienvenido!',
+      position: 'topRight'
+    });
+
+    const mensaje = encodeURIComponent(mensajeInput.value); // Codifica el mensaje
+    const numero = '+525586761828'; // Tu número de WhatsApp
     const enlaceWhatsApp = `https://api.whatsapp.com/send?phone=${encodeURIComponent(numero)}&text=${(mensaje)}`;
-    window.open(enlaceWhatsApp, '_blank');
-    console.log(enlaceWhatsApp);
+
+    // Redirige a WhatsApp después de 1.5 segundos (
+    setTimeout(function() {
+      window.open(enlaceWhatsApp, '_blank');
+    }, 1500);
   });
 });
