68d20a74f037b10ca9bcc058ecef16e1ed9e9447
LanSang69 <alanisg029@gmail.com>
2023-09-11 08:09:30 -0600

    Adding new changes to about us page and design

---
+++ js/mensajeWhatsapp.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Asegura que los elementos existan antes de hacer algo
+  const mensajeInput = document.getElementById('mensajeInput'); // Obtengo el ID del mensaje
+  const enviarButton = document.getElementById('enviarButton'); // Obtengo ID del botón
+
+  enviarButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe por defecto
+
+    iziToast.success({
+      title: 'Redirigiendo a WhatsApp...',
+      message: '¡Bienvenido!',
+      position: 'topRight'
+    });
+
+    const mensaje = encodeURIComponent(mensajeInput.value); // Codifica el mensaje
+    const numero = '+525586761828'; // Tu número de WhatsApp
+    const enlaceWhatsApp = `https://api.whatsapp.com/send?phone=${encodeURIComponent(numero)}&text=${(mensaje)}`;
+
+    // Redirige a WhatsApp después de 1.5 segundos (
+    setTimeout(function() {
+      window.open(enlaceWhatsApp, '_blank');
+    }, 1500);
+  });
+});
--- js/menuDesplegable.js
+++ js/menuDesplegable.js
@@ -1,21 +1,26 @@
 (function() {
-    const menuButton = document.querySelector('.nav__menu');
-    const menu = document.querySelector('.nav__link--menu');
-    const menuItems = menu.querySelectorAll('.nav__links');
-  
-    function toggleMenu() {
-      menu.classList.toggle('nav__link--show');
-      menuButton.classList.toggle('open');
-    }
-  
-    function closeMenu() {
-      menu.classList.remove('nav__link--show');
-      menuButton.classList.remove('open');
-    }
-  
-    menuButton.addEventListener('click', toggleMenu);
-  
-    menuItems.forEach(item => {
-      item.addEventListener('click', closeMenu);
-    });
-  })();
+  // Seleccionando el botón del menú y el menú en sí
+  const menuButton = document.querySelector('.nav__menu');
+  const menu = document.querySelector('.nav__link--menu');
+  const menuItems = menu.querySelectorAll('.nav__links');
+
+  // Función para alternar la visibilidad del menú
+  function toggleMenu() {
+    menu.classList.toggle('nav__link--show'); // Alterna la clase 'nav__link--show' en el menú
+    menuButton.classList.toggle('open'); // Alterna la clase 'open' en el botón del menú
+  }
+
+  // Función para cerrar el menú
+  function closeMenu() {
+    menu.classList.remove('nav__link--show'); // Elimina la clase 'nav__link--show' del menú
+    menuButton.classList.remove('open'); // Elimina la clase 'open' del botón del menú
+  }
+
+  // Agregar un evento de escucha al botón del menú para alternar el menú
+  menuButton.addEventListener('click', toggleMenu);
+
+  // Agregar eventos de escucha a cada elemento del menú para cerrar el menú cuando se hace clic en un enlace
+  menuItems.forEach(item => {
+    item.addEventListener('click', closeMenu);
+  });
+})();
